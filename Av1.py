8c9c604765775b30d7fb7da673be7d50af9d114b
Rebeca <84825856+soubeca@users.noreply.github.com>
2025-04-05 01:22:07 -0300

    Update Av1.py

--- Av1.py
+++ Av1.py
@@ -3,7 +3,6 @@
 # Por fim, exiba o conteúdo da tupla resultante para verificar as frutas que foram armazenadas.
 
 list_frut = ["banana", "uva", "morango", "manga", "graviola", "laranja"]
-# o * coloca os valores da lista para fora do [], convertendo em uma tupla
 tuple_frut = (*list_frut,)
 
 print(tuple_frut)
