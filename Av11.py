30cc0cef2a0186d7bce979fd6666c9c185b6f2fc
Rebeca <84825856+soubeca@users.noreply.github.com>
2025-07-26 00:00:35 -0300

    Update Av11.py
    Atividade POO I

--- Av11.py
+++ Av11.py
@@ -1,1 +1,15 @@
+class Animal:
+    def falar(self):
+        print('Este animal faz um som genérico.')
 
+class Cachorro:
+    def falar(self):
+        print('O cachorro está latindo.🐶')
+
+class Gato:
+    def falar(self):
+        print('O gato está miando.')
+
+objAnimal = Animal().falar()
+objCachorro = Cachorro().falar()
+objGato = Gato().falar()
